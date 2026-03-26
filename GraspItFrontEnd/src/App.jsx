d39510baef0f59a19b2c66c77f21aec211c6d860
Kamogelo Moswathupa <99497643+devkamogelo@users.noreply.github.com>
2026-03-26 12:46:18 +0200

    Change the analytics to the react package

--- GraspItFrontEnd/src/App.jsx
+++ GraspItFrontEnd/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
